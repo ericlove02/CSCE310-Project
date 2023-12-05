1180f665a6b48cf3906a8ee5e514109eca73bc6e
Joel Herzog <11185290+grubsteak@users.noreply.github.com>
2023-12-05 15:40:32 -0600

    Make student info update update user as well as student fields

--- utils/update.php
+++ utils/update.php
@@ -3,21 +3,37 @@ session_start();
 
 require 'connect.php';
 
-$id = $_SESSION['user_id'];
-$f_name = $_POST['f_name'];
-$l_name = $_POST['l_name'];
-$email = $_POST['email'];
 
+$id = $_SESSION['user_id']; 
 // TODO: Add validation and sanitization
 
-$sql = "UPDATE users SET f_name = '$f_name', l_name = '$l_name', email = '$email' WHERE user_id = '$id'";
+$userkeys = array('user_id' => true, 'email' => true, 'f_name' => true, 'l_name' => true, 'm_initial'=> true, 'phone' => true, 'password' => true, 'is_admin' => true);
+
+$userUpdates = "";
+$stuUpdates = "";
+$userValues = [];
+$stuValues = [];
+foreach($_POST as $key => $value) {
+    if(@$userkeys[$key]) {
+        $userUpdates .= "$key = ?, ";
+        array_push($userValues, $value);
+    } else {
+        $stuUpdates .= "$key = ?, ";
+        array_push($stuValues, $value);
+    }
+}
+$userUpdates = substr($userUpdates,0,-2);
+$stuUpdates = substr($stuUpdates,0,-2);
+
+
+$sql = "UPDATE users SET " . $userUpdates . " WHERE user_id = '$id';";
+$sql2 = "UPDATE students SET " . $stuUpdates . " WHERE user_id = '$id'";
 
 header("Location: ../student.php");
-// if ($conn->query($sql) === TRUE) {
-//     echo "Record updated successfully";
-// } else {
-//     echo "Error updating record: " . $conn->error;
-// }
-
+if ($conn->execute_query($sql, $userValues) === TRUE && $conn->execute_query($sql2, $stuValues) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
 $conn->close();
 ?>
