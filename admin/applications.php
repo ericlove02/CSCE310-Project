d126a2bff542a0db944357aa03cff0cc6b4cae28
Eric Love <eric.love02@yahoo.com>
2023-12-07 13:23:53 -0600

    admin view update, navbar and pages

---
+++ admin/applications.php
@@ -0,0 +1,162 @@
+<?php
+require_once "../utils/connect.php";
+
+// check if page was psoted to
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['update_app'])) {
+        $selectedRecordId = $_POST['selected_record_id'];
+        $isApproved = $_POST['is_approved'];
+
+        // get application details based on app_id
+        $sqlSelect = "SELECT * FROM applications WHERE app_id = $selectedRecordId";
+        $resultSelect = $conn->query($sqlSelect);
+
+        if ($resultSelect) {
+            $applicationDetails = $resultSelect->fetch_assoc();
+
+            if ($isApproved) {
+                // insert into programenrollments
+                $user_id = $applicationDetails['user_id'];
+                $prog_id = $applicationDetails['prog_id'];
+
+                $sqlInsert = "INSERT INTO programenrollments (user_id, prog_id) VALUES ($user_id, $prog_id)";
+                $resultInsert = $conn->query($sqlInsert);
+
+                if (!$resultInsert) {
+                    die("Insert into programenrollments failed: " . $conn->error);
+                }
+            }
+
+            // delete the applciations either way
+            $sqlDelete = "DELETE FROM applications WHERE app_id = $selectedRecordId";
+            $resultDelete = $conn->query($sqlDelete);
+
+            if (!$resultDelete) {
+                die("Delete from applications failed: " . $conn->error);
+            }
+
+            echo "Application updated successfully.";
+        } else {
+            die("Select application details failed: " . $conn->error);
+        }
+    }
+}
+
+// get all aplications along with their user and program data
+$sql = "SELECT applications.*, users.*, programs.* 
+            FROM applications 
+            JOIN users ON applications.user_id = users.user_id 
+            JOIN programs ON applications.prog_id = programs.prog_id";
+$result = $conn->query($sql);
+$applications = $result->fetch_all(MYSQLI_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+    <link rel="stylesheet" href="/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <span class="navbar-brand">Admin Page</span>
+
+            <!-- Navbar links -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="applications.php">Applications</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="certifications.php">Certifications</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="courses.php">Courses</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="events.php">Events</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="programs.php">Programs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="stats.php">Stats</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="users.php">Users</a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- Logout button on right side -->
+            <div class="navbar-nav ms-auto">
+                <a href="../utils/logout.php" class="btn btn-danger">Logout</a>
+            </div>
+        </div>
+    </nav>
+    <section>
+        <h3>Review Applications</h3>
+        <table border="1">
+            <tr>
+                <th>Application Id</th>
+                <th>User First Name</th>
+                <th>User Last Name</th>
+                <th>Program Name</th>
+                <th>Purpose Statement</th>
+                <th>Uncompleted Certifications</th>
+                <th>Completed Certifications</th>
+            </tr>
+
+            <?php
+            foreach ($applications as $application) {
+                echo "<tr>";
+                echo "<td><span>{$application['app_id']}</span></td>";
+                echo "<td><span>{$application['f_name']}</span></td>";
+                echo "<td><span>{$application['l_name']}</span></td>";
+                echo "<td><span>{$application['prog_name']}</span></td>";
+                echo "<td><span>{$application['app_purpose_statement']}</span></td>";
+                echo "<td><span>{$application['uncom_cert']}</span></td>";
+                echo "<td><span>{$application['com_cert']}</span></td>";
+                echo "</tr>";
+            }
+            ?>
+        </table>
+
+        <br>
+        <h4>Modify Application</h4>
+
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <input type="hidden" name="selected_table" value="applications">
+
+            <label>Select Application:</label>
+            <select name="selected_record_id">
+                <?php
+                foreach ($applications as $application) {
+                    echo "<option value='{$application['app_id']}'>{$application['app_id']} - {$application['f_name']} {$application['l_name']}, {$application['prog_name']}</option>";
+                }
+                ?>
+            </select>
+            <br>
+
+            <select name="is_approved" id="is_approved">
+                <option value=""></option>
+                <option value="1">Approve</option>
+                <option value="0">Deny</option>
+            </select>
+
+            <br><br>
+            <button type="submit" name="update_app" class="btn btn-dark">Update Application</button>
+        </form>
+    </section>
+    <?php
+    // Close connection
+    $conn->close();
+    ?>
+</body>
+
+</html>
---
+++ admin/courses.php
@@ -0,0 +1,264 @@
+<?php
+require_once "../utils/connect.php";
+
+// select all entities from a table
+function getAllRecords($conn, $tableName)
+{
+    $sql = "SELECT * FROM $tableName";
+    $result = $conn->query($sql);
+    if (!$result) {
+        die("Query failed: " . $conn->error);
+    }
+    $records = array();
+    while ($row = $result->fetch_assoc()) {
+        $records[] = $row;
+    }
+    return $records;
+}
+
+// update a table given the table name and a dict of values
+function updateRecord($conn, $tableName, $recordId, $recordData)
+{
+    $updateValues = '';
+    // parse value pairs
+    foreach ($recordData as $column => $value) {
+        if (strpos($column, '_id') !== false) {
+            $id_key = $column;
+            continue;
+        }
+        if ($value != '') {
+            $updateValues .= "$column = '$value', ";
+        }
+    }
+    $updateValues = rtrim($updateValues, ', ');
+
+    $sql = "UPDATE $tableName SET $updateValues WHERE $id_key=$recordId";
+    if ($conn->query($sql) !== TRUE) {
+        echo "Error updating record: " . $conn->error;
+    } else {
+        echo "Entry in $tableName updated";
+    }
+}
+
+// insert into given table name with dict of values
+function addRecord($conn, $tableName, $recordData)
+{
+    foreach ($recordData as $key => $value) {
+        if ($value == "add_new") {
+            unset($recordData[$key]);
+        }
+    }
+    $columns = implode(', ', array_keys($recordData));
+    $values = "'" . implode("', '", array_values($recordData)) . "'";
+    $sql = "INSERT INTO $tableName ($columns) VALUES ($values)";
+    if ($conn->query($sql) !== TRUE) {
+        echo "Error adding new record: " . $conn->error;
+    } else {
+        echo "New entry to $tableName added";
+    }
+}
+
+// check if page was psoted to
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // if posted to update a record
+    if (isset($_POST['update_record'])) {
+        $selectedRecordId = $_POST['selected_record_id'];
+        $recordData = array();
+
+        // handle data based on which table was selected
+        switch ($_POST['selected_table']) {
+            case 'users':
+                $recordData['user_id'] = $_POST['selected_record_id'];
+                $recordData['f_name'] = $_POST['first_name'];
+                $recordData['l_name'] = $_POST['last_name'];
+                $recordData['m_initial'] = $_POST['m_initial'];
+                $recordData['phone'] = $_POST['phone'];
+                $recordData['password'] = $_POST['password'];
+                $recordData['is_admin'] = $_POST['is_admin'];
+                break;
+            case 'events':
+                $recordData['event_id'] = $_POST['selected_record_id'];
+                $recordData['event_name'] = $_POST['record_name'];
+                $recordData['event_location'] = $_POST['record_loc'];
+                break;
+            case 'trainings':
+                $recordData['train_id'] = $_POST['selected_record_id'];
+                $recordData['train_name'] = $_POST['record_name'];
+                break;
+            case 'certifications':
+                $recordData['cert_id'] = $_POST['selected_record_id'];
+                $recordData['cert_name'] = $_POST['record_name'];
+                break;
+            case 'programs':
+                $recordData['prog_id'] = $_POST['selected_record_id'];
+                $recordData['prog_name'] = $_POST['record_name'];
+                break;
+            case 'courses':
+                $recordData['cour_id'] = $_POST['selected_record_id'];
+                $recordData['cour_name'] = $_POST['record_name'];
+                break;
+
+            case 'summercamps':
+                $recordData['camp_id'] = $_POST['selected_record_id'];
+                $recordData['sc_name'] = $_POST['record_name'];
+                $recordData['sc_year'] = $_POST['record_year'];
+                break;
+            default:
+                // default case
+                echo "Invalid table selected";
+                break;
+        }
+
+        // if add_new its a new record
+        if ($selectedRecordId == 'add_new') {
+            addRecord($conn, $_POST['selected_table'], $recordData);
+        } else {
+            // update existing record
+            updateRecord($conn, $_POST['selected_table'], $selectedRecordId, $recordData);
+        }
+    } elseif (isset($_POST['delete_record'])) {
+        $selectedRecordId = $_POST['selected_record_id'];
+        $selectedTable = $_POST['selected_table'];
+        if ($selectedRecordId == 'add_new') {
+            // show an alert when trying to delete 'add new'
+            echo '<script>alert("Error: Cannot delete a new record.");</script>';
+        } else {
+            switch ($_POST['selected_table']) {
+                case 'users':
+                    $sql = "DELETE FROM $selectedTable WHERE user_id = $selectedRecordId";
+                    break;
+                case 'events':
+                    $sql = "DELETE FROM $selectedTable WHERE event_id = $selectedRecordId";
+                    break;
+                case 'trainings':
+                    $sql = "DELETE FROM $selectedTable WHERE train_id = $selectedRecordId";
+                    break;
+                case 'certifications':
+                    $sql = "DELETE FROM $selectedTable WHERE cert_id = $selectedRecordId";
+                    break;
+                case 'programs':
+                    $sql = "DELETE FROM $selectedTable WHERE prog_id = $selectedRecordId";
+                    break;
+                case 'courses':
+                    $sql = "DELETE FROM $selectedTable WHERE cour_id = $selectedRecordId";
+                    break;
+                case 'summercamps':
+                    $sql = "DELETE FROM $selectedTable WHERE camp_id = $selectedRecordId";
+                    break;
+                default:
+                    // default case
+                    echo "Invalid table selected";
+                    break;
+            }
+            if ($conn->query($sql) !== TRUE) {
+                echo "Error deleting record: " . $conn->error;
+            } else {
+                echo "$selectedTable record deleted";
+            }
+        }
+    }
+}
+
+// get all records from each table
+$courses = getAllRecords($conn, 'courses');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+    <link rel="stylesheet" href="/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <span class="navbar-brand">Admin Page</span>
+
+            <!-- Navbar links -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="applications.php">Applications</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="certifications.php">Certifications</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="courses.php">Courses</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="events.php">Events</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="programs.php">Programs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="stats.php">Stats</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="users.php">Users</a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- Logout button on right side -->
+            <div class="navbar-nav ms-auto">
+                <a href="../utils/logout.php" class="btn btn-danger">Logout</a>
+            </div>
+        </div>
+    </nav>
+    <section>
+        <h3>Courses</h3>
+        <table border="1">
+            <tr>
+                <th>Course Id</th>
+                <th>Course Name</th>
+            </tr>
+
+            <?php
+            foreach ($courses as $course) {
+                echo "<tr>";
+                echo "<td><span>{$course['cour_id']}</span></td>";
+                echo "<td><span>{$course['cour_name']}</span></td>";
+                echo "</tr>";
+            }
+            ?>
+
+        </table>
+
+        <br>
+        <h4>Modify Course</h4>
+
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <input type="hidden" name="selected_table" value="courses">
+
+            <label>Select Course:</label>
+            <select name="selected_record_id">
+                <option value="add_new">Add new Course</option>
+                <?php
+                foreach ($courses as $course) {
+                    echo "<option value='{$course['cour_id']}'>{$course['cour_id']} - {$course['cour_name']}</option>";
+                }
+                ?>
+            </select>
+            <br>
+
+            <label>Course Name:</label>
+            <input type="text" name="record_name"><br>
+
+            <br>
+            <button type="submit" name="update_record" class="btn btn-dark">Update/Add</button>
+            <button type="submit" name="delete_record" class="btn btn-dark">Delete</button>
+        </form>
+    </section>
+    <?php
+    // Close connection
+    $conn->close();
+    ?>
+</body>
+
+</html>
